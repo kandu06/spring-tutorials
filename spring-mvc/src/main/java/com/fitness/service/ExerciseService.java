65fa5d34cdaffd6d5ea2a5024031ae544907f973
kandasamy thangavelu <kandu06@gmail.com>
2017-04-03 22:44:17 -0400

    adding ContentNegotiatingViewResolver & some basic jquery to process json response

---
+++ spring-mvc/src/main/java/com/fitness/service/ExerciseService.java
@@ -0,0 +1,11 @@
+package com.fitness.service;
+
+import java.util.List;
+
+import com.fitness.model.Activity;
+
+public interface ExerciseService {
+
+	List<Activity> findAllActivities();
+
+}
---
+++ spring-mvc/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<b><a href="addGoal.html">Add Goal</a></b>
+</body>
+</html>
