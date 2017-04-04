e8e62735fe91ecf8da4cfef8514bf95d0bb7e6cb
kandasamy thangavelu <kandu06@gmail.com>
2017-04-03 22:44:05 -0400

    adding ContentNegotiatingViewResolver & some basic jquery to process json response

--- spring-mvc/src/main/java/com/fitness/controller/GoalController.java
+++ spring-mvc/src/main/java/com/fitness/controller/GoalController.java
@@ -12,26 +12,33 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.fitness.model.Goal;
 
+
+
 @Controller
-@SessionAttributes(names="goal") //adds goal object to session.
+@SessionAttributes("goal")
 public class GoalController {
-	
-	@RequestMapping(value="addGoal", method=RequestMethod.GET)
-	public String addGoal(Model model){
-		Goal goal=new Goal();
-		goal.setMinutes(10); //set the default value here. When the url is hit by default 10 will be shown.
+
+	@RequestMapping(value = "addGoal", method = RequestMethod.GET)
+	public String addGoal(Model model) {
+		Goal goal = new Goal();
+		goal.setMinutes(10);
 		model.addAttribute("goal", goal);
+		
 		return "addGoal";
 	}
-
-	@RequestMapping(value="addGoal", method=RequestMethod.POST)
-	public String updateGoal(@Valid @ModelAttribute("goal") Goal goal, BindingResult result){ //@Valid tells to run validation and the results of the validation is returned in BindingResult object.
+	
+	@RequestMapping(value = "addGoal", method = RequestMethod.POST)
+	public String updateGoal(@Valid @ModelAttribute("goal") Goal goal, BindingResult result) {
 		
-		if(result.hasErrors()){
-			return "addGoal"; //uses Internal Resource View Resolver.
+		System.out.println("result has errors: " + result.hasErrors());
+		
+		System.out.println("Minutes updated: " + goal.getMinutes());
+		
+		if(result.hasErrors()) {
+			return "addGoal";
 		}
 		
 		return "redirect:addMinutes.html";
 	}
-
+	
 }
--- spring-mvc/src/main/java/com/fitness/controller/MinutesController.java
+++ spring-mvc/src/main/java/com/fitness/controller/MinutesController.java
@@ -1,19 +1,59 @@
 package com.fitness.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fitness.model.Activity;
 import com.fitness.model.Exercise;
+import com.fitness.service.ExerciseService;
 
 @Controller
 public class MinutesController {
 	
+	@Autowired
+	private ExerciseService exerciseService;
+	
 	@RequestMapping(value = "/addMinutes")
-	public String addMinutes(@ModelAttribute("exercise") Exercise exercise){
+	public String addMinutes(@Valid @ModelAttribute("exercise") Exercise exercise, BindingResult result){
 		
-		System.out.println("Exercised Minutes:"+exercise.getMinutes());
+		System.out.println("exercise: " + exercise.getMinutes());
+		System.out.println("exercise activity: " + exercise.getActivity());
+		
+		//This will use internal view resolver to get the correct view. in this case WEB-INF/jsp/addMinutes.html
 		return "addMinutes";
+		
+		// This will forward the request object and minutes in exercise object will be preserved. 
+		// NOTE: .html in the end, it is needed, because it does not use internal view resolver, but goes as a new request and gets resolved in web.xml. 
+//		return "forward:addMoreMinutes.html"; 
+		
+		
+		// This will redirect the request to browser and then back to addMoreMinutes.html. Hence the minutes in exercise object is lost.
+		// NOTE: .html in the end, it is needed, because it does not use internal view resolver, but goes as a new request and gets resolved in web.xml. 
+				
+//		return "redirect:addMoreMinutes.html"; 
 	}
 
+	@RequestMapping(value = "/addMoreMinutes")
+	public String addMoreMinutes(@ModelAttribute("exercise") Exercise exercise){
+		
+		System.out.println("Exercising:"+exercise.getMinutes());
+		return "addMinutes";
+	}
+	
+	@RequestMapping(value ="/activities", method =RequestMethod.GET) //spring-mvc/activites.json or spring-mvc/activites.xml
+	public @ResponseBody /*means return is the entire response itself, does not go to another view.*/ List<Activity> ListAllActivities(){
+		
+		return exerciseService.findAllActivities();
+	}
+	
+	
 }
